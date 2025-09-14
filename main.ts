0961004f1bf352558c91da331b13f43d5cff8da9
Phil <philly.craig@gmail.com>
2025-09-14 19:00:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "stand") {
-        xgo.execution_action(xgo.action_enum.Stand)
+    if (receivedString == "savePose") {
+        xgo.GetPosestate(xgo.pose_enum.pose1)
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "joystick_x") {
+        if (value < 0) {
+            xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_left, value, 0.1)
+        } else {
+            xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_right, value, 0.1)
+        }
+    } else if (name == "joystick_y") {
+        if (value > 0) {
+            xgo.translational_step(xgo.translation_direction_enum.Forward, value)
+        } else {
+            xgo.translational_step(xgo.translation_direction_enum.Backward, value)
+        }
     }
 })
 xgo.init_xgo_serial(SerialPin.P14, SerialPin.P13)
